ce39c905b01006f52cfca744e83d328d54748236
Yohan de Rose <yohanderose@gmail.com>
2020-08-03 13:28:00 +1000

    removed install requirements in setup.py

--- setup.py
+++ setup.py
@@ -20,11 +20,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=[
-        'geopandas',
-        'numpy',
-        'pandas',
-        'map2model-loop3d'
-    ],
     python_requires='>=3.6',
 )
