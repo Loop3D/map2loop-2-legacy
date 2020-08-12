e9e915aee407f460570a2a6a6ab2b253c628e6c1
Yohan de Rose <yohanderose@gmail.com>
2020-08-12 20:53:03 +1200

    Updating again, 1.04 was an existing install

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="map2loop",
-    version="1.0.4",
+    version="1.0.5",
     author="Yohan de Rose",
     author_email="contact@loop3d.org",
     description="Generate 3D model data using 2D maps.",
