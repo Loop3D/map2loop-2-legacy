34518e2d1ffccec8142c60bcb0fb2edeceae0f48
markjessell <markjessell@users.noreply.github.com>
2022-04-28 10:29:30 +0800

    fix fault dip direction code

--- map2loop/m2l_geometry.py
+++ map2loop/m2l_geometry.py
@@ -757,7 +757,7 @@ def save_faults(path_faults, output_path, dtm, dtb, dtb_null, cover_map, c_l, fa
                         if(int(float(flt[c_l['fdip']])) == int(float(c_l['fdipnull']))):
                             # dip estimate defined
                             if(not str(flt[c_l['fdipest']]) == '-999'):
-                                #print("1")
+                                print("1")
                                 i = 0
                                 for choice in split:
                                     if(flt[c_l['o']] == '-1'):
@@ -769,20 +769,20 @@ def save_faults(path_faults, output_path, dtm, dtb, dtb_null, cover_map, c_l, fa
                                     i = i+1
                             else:
                                 if(flt[c_l['fdip']] == -999 or fault_dip_var == -999):  # random flag
-                                    #print("2")
+                                    print("2")
                                     fault_dip = random.randint(60, 90)
                                 else:
-                                    #print("3")
+                                    print("3")
                                     fault_dip = fault_dip_var
                         else:
-                            #print("4")
+                            print("4")
                             # specific dip defined
                             fault_dip = int(float(flt[c_l['fdip']]))
 
 
                         # numeric dip direction defined
                         if(c_l['fdipdir_flag'] == 'num'  ):
-                            if( pd.notna( flt[c_l['fdipdir']])):
+                            if( pd.notna( flt[c_l['fdipdir']]) and flt[c_l['fdipdir']]!='-999'):
                                 azimuth = flt[c_l['fdipdir']]
                             else:
                                 azimuth=azimuth_fault                            
@@ -791,14 +791,15 @@ def save_faults(path_faults, output_path, dtm, dtb, dtb_null, cover_map, c_l, fa
                             #print('az_before',fault_name,azimuth_fault)
                             azimuth = (azimuth_fault+(180*random.randint(0,1)))%360
                             #print('az_after',fault_name,azimuth)
-                        elif (not str(flt[c_l['fdipdir']]) == 'None' and not str(float(flt[c_l['fdip']])) == c_l['fdipnull']):
+                        elif (not str(flt[c_l['fdipdir']]) == 'None' and not str(float(flt[c_l['fdip']])) == c_l['fdipnull'] and flt[c_l['fdipdir']]!='-999'):
                             dotprod = degrees(acos(
-                                (-lsx*dip_dirs[flt[c_l['fdipdir']]][0])+(lsy*dip_dirs[flt[c_l['fdipdir']]][1])))
+                                (-dlsx*dip_dirs[flt[c_l['fdipdir']]][0])+(dlsy*dip_dirs[flt[c_l['fdipdir']]][1])))
                             if(dotprod > 45):
                                 fault_dip = -fault_dip
                         else:
                             azimuth=azimuth_fault
-
+                        #print("indx,azimuth,azimuth_fault,flt[c_l['fdipdir']],pd.notna( flt[c_l['fdipdir']]),fault_dip_var, c_l['fdipnull'],flt[c_l['fdip']]")
+                        #print( indx,azimuth,azimuth_fault,flt[c_l['fdipdir']],pd.notna( flt[c_l['fdipdir']]),fault_dip_var, c_l['fdipnull'],flt[c_l['fdip']])
                         l,m,n=m2l_utils.ddd2dircos((90-fault_dip),azimuth)
                         #print('fault_name,l,m,n,azimuth_fault,dip',fault_name,l,m,n,azimuth,fault_dip)
                         first = True
